5d67be2127690713a9275ad918ba269a8e7dd69b
awwa500@gmail.com <awwa500@gmail.com>
2014-06-05 23:42:26 +0900

    First commit

---
+++ GAEProject/src/com/google/com/GAEProjectServlet.java
@@ -0,0 +1,11 @@
+package com.google.com;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GAEProjectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
